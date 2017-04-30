9ef425d8d9e24731d024eea25cc544dc684840e4
pungggi <alessandro.pungitore@gmail.com>
2017-04-30 21:45:50 +0200

    d

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const shell = require('shelljs')
 const os = require('os')
+const fs = require('fs')
+
 console.log(`You are workin on ${process.platform}
-your homedir is: ${os.homedir}
-whereas your tempdir: ${os.tmpdir}`)
+your homedir is: ${os.homedir()}
+whereas your tempdir: ${os.tmpdir()}`)
 
 exports.printMsg = () => {
     shell.exec("npm -v")
