da160311b9c5bb680edc67e3cd3b988d1fd9b6ff
pungggi <alessandro.pungitore@gmail.com>
2017-04-30 23:52:10 +0200

    fsdf

--- index.js
+++ index.js
@@ -4,36 +4,38 @@ const fs = require('fs-extra')
 const path = require('path')
 const readline = require('readline')
 
-console.log(`You are workin on ${process.platform}
-your homedir is: ${os.homedir()}
-whereas your tempdir is: ${os.tmpdir()}`)
+// console.log(`You are workin on ${process.platform}
+// your homedir is: ${os.homedir()}
+// whereas your tempdir is: ${os.tmpdir()}`)
 
 exports.printMsg = () => {
     shell.exec("npm -v")
 }
 
-exports.dev = async (
-    input = path.resolve(process.cwd(), ".env"),
-    command = 'next') => {
+exports.dev =
+    (
+        command = 'next',
+        input = path.resolve(process.cwd(), ".env")
+    ) => {
+        console.log(input)
+        let script = ""
+        const dotenv = readline.createInterface({
+            input: fs.createReadStream(input)
+        })
 
-    let script = ""
-    const dotenv = readline.createInterface({
-        input: fs.createReadStream(input)
-    })
-
-    dotenv.on("line", line => {
-        if (line.includes("=")) {
-            script += `set ${line} \n`
-        }
-    })
-    dotenv.on("close", () => {
-        script += command
-        // console.log(script)
-        // shell.exec(script)
-        readline.clearLine(dotenv, 0)
-        Promise.resolve(script)
-    })
-}
+        dotenv.on("line", line => {
+            if (line.includes("=")) {
+                script += `set ${line} \n`
+            }
+        })
+        dotenv.on("close", () => {
+            script += command
+            console.log(script)
+            shell.exec(script)
+            readline.clearLine(dotenv, 0)
+            Promise.resolve(script)
+        })
+    }
 
 exports.createdeploy = async () => {
     fs.outputFile(path.resolve(__dirname, "deploy.js"),
