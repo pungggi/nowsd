48d36b3f19f9c37d6cb0d57ed3d22b8fdbeb4a64
pungggi <alessandro.pungitore@gmail.com>
2017-05-01 10:28:12 +0200

    src

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const shell = require('shelljs')
 const os = require('os')
 const fs = require('fs-extra')
 const path = require('path')
 const readline = require('readline')
-
-// console.log(`You are workin on ${process.platform}
-// your homedir is: ${os.homedir()}
-// whereas your tempdir is: ${os.tmpdir()}`)
-
-exports.printMsg = () => {
-    shell.exec("npm -v")
-}
 
 exports.dev =
     (
@@ -18,29 +9,52 @@ exports.dev =
         input = path.resolve(process.cwd(), ".env")
     ) => {
         return new Promise((resolve, reject) => {
-            console.log(input)
             let script = ""
-            const dotenv = readline.createInterface({
+            const src = readline.createInterface({
                 input: fs.createReadStream(input)
             })
 
-            dotenv.on("line", line => {
+            src.on("line", line => {
                 if (line.includes("=")) {
-                    script += `set ${line} & `
+                    if (os.platform == 'win32') {
+                        script += `set `
+                    }
+                    script += `${line} & `
                 }
             })
-            dotenv.on("close", () => {
+            src.on("close", () => {
                 script += `${command}`
-                // console.log(script)
-                shell.exec(script)
-                readline.clearLine(dotenv, 0)
+                readline.clearLine(src, 0)
                 resolve(script)
             })
         })
     }
 
-exports.createdeploy = async () => {
-    fs.outputFile(path.resolve(__dirname, "deploy.js"),
-        `x`)
-}
+exports.deploy =
+    (
+        command = 'now',
+        input = path.resolve(process.cwd(), ".env")
+    ) => {
+        return new Promise((resolve, reject) => {
+            let script = `${command} `
+            const src = readline.createInterface({
+                input: fs.createReadStream(input)
+            })
 
+            src.on("line", line => {
+                if (line.includes("=")) {
+                    script += `-e ${line}`
+                }
+            })
+            src.on("close", () => {
+                script += 
+                readline.clearLine(src, 0)
+                resolve(script)
+            })
+        })
+    }
+
+// exports.createdeploy = async () => {
+//     fs.outputFile(path.resolve(__dirname, "deploy.js"),
+//         `x`)
+// }
