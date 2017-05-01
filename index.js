d5371eae6ac9a0130a627d9e697d7c04d50bc9b5
pungggi <alessandro.pungitore@gmail.com>
2017-05-01 23:44:26 +0200

    done

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const os = require('os')
 const fs = require('fs-extra')
 const path = require('path')
 const readline = require('readline')
-const now = require('now-client')('6FuTYkrAFe6lLFOvMV7CwRQ9')
-now.getSecrets()
-    .then(secrets => {
-        console.log(secrets)
-    })
-    .catch(error => {
-        console.error(error)
-    })
+const nowClient = require('now-client')
+const { findIndex } = require('lodash')
+
+let nowToken = ""
+_addSecret = async (name, value) => {
+    console.log(` add ${name} to ${value}`)
+    let data = await nowClient(nowToken).createSecret(name, value)
+}
+_resetSecret = async (name, value) => {
+    console.log(` delete ${name} to ${value}`)
+    let data = await nowClient(nowToken).deleteSecret(name, value)
+    console.log(` add ${name} to ${value}`)
+    data = await nowClient(nowToken).createSecret(name, value)
+}
 
 exports.dev =
     (
@@ -44,26 +50,43 @@ exports.deploy =
         command = 'now',
         input = path.resolve(process.cwd(), ".env")
     ) => {
-        return new Promise((resolve, reject) => {
-            if(!token)
-            {
-                reject('Token missing. See https://zeit.co/account/tokens.')
-            }
-            let script = `${command} `
-            const source = readline.createInterface({
-                input: fs.createReadStream(input)
-            })
-
-            source.on("line", line => {
-                if (line.includes("=")) {
-                    script += `-e ${line} `
+        return new Promise(
+            (
+                resolve,
+                reject
+            ) => {
+                if (!token) {
+                    reject('Token missing. See https://zeit.co/account/tokens.')
                 }
+                nowToken = token
+                nowClient(nowToken).getSecrets()
+                    .then(secrets => {
+                        let script = `${command} `
+                        const source = readline.createInterface({
+                            input: fs.createReadStream(input)
+                        })
+                        // for (let secret of secrets) {
+                        //     console.log(secret.name)
+                        // }
+                        source.on("line", line => {
+                            if (line.includes("=")) {
+                                let envVar = line.split("=")[0].toLowerCase()
+                                let indexFound = findIndex(secrets, ['name', envVar])
+                                indexFound == -1 ?
+                                    _addSecret(envVar, line.split("=")[1]) :
+                                    _resetSecret(envVar, line.split("=")[1])
+                                script += `-e ${envVar}=@${envVar} `
+                            }
+                        })
+                        source.on("close", () => {
+                            readline.clearLine(source, 0)
+                            resolve(script)
+                        })
+                    })
+                    .catch(error => {
+                        console.error(error)
+                    })
             })
-            source.on("close", () => {
-                readline.clearLine(source, 0)
-                resolve(script)
-            })
-        })
     }
 
 // exports.createdeploy = async () => {
