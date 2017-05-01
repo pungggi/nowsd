7c93b4cd550099ba5a892ed8b80de6c296fcd108
pungggi <alessandro.pungitore@gmail.com>
2017-05-01 17:02:58 +0200

    token

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const os = require('os')
 const fs = require('fs-extra')
 const path = require('path')
 const readline = require('readline')
+const now = require('now-client')('6FuTYkrAFe6lLFOvMV7CwRQ9')
+now.getSecrets()
+    .then(secrets => {
+        console.log(secrets)
+    })
+    .catch(error => {
+        console.error(error)
+    })
 
 exports.dev =
     (
@@ -32,22 +40,27 @@ exports.dev =
 
 exports.deploy =
     (
+        token,
         command = 'now',
         input = path.resolve(process.cwd(), ".env")
     ) => {
         return new Promise((resolve, reject) => {
+            if(!token)
+            {
+                reject('Token missing. See https://zeit.co/account/tokens.')
+            }
             let script = `${command} `
-            const src = readline.createInterface({
+            const source = readline.createInterface({
                 input: fs.createReadStream(input)
             })
 
-            src.on("line", line => {
+            source.on("line", line => {
                 if (line.includes("=")) {
                     script += `-e ${line} `
                 }
             })
-            src.on("close", () => {
-                readline.clearLine(src, 0)
+            source.on("close", () => {
+                readline.clearLine(source, 0)
                 resolve(script)
             })
         })
